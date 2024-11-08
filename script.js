9a5e4ea1247267a67f53a9c92f3728ce216089df
ANSH KUMAR <anshkumar1399@gmail.com>
2024-11-09 01:03:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// let a=7;
+// button
+// switch(a){
+// case 1:
+//     document.write('case 1');
+//     break;
+// case 2:
+//     document.write('case 2');
+//     break;
+// case 3:
+//     document.write('case 3');
+//     break;
+// case 4:
+//     document.write('case 4');
+//     break;
+
+// loop
+// for (let i = 1; i <= 5; i++) {
+//     document.write(i + "<br>");
+// }
+// let j = 6;
+// while (j <= 10) {
+//     j++;
+//     document.write(j + "<br>");
+// }
+// let k=12;
+// do{
+//     document.write(k + "<br>");
+//     k++;
+// }while(k<=15);
+// }
+// for(let i=1; i<=5; i++){
+//     if (i==3){
+//         alert("continue on 3")
+//         continue;
+//     }document.write(i +"<br>")
+// }
+var a = Number(prompt('enter 1st number'));
+var b = Number(prompt('enter 2nd number'));
+alert('sum is:' + (a+b));
